db5b1de6c995afc22ca71b19bb599700b3786697
Anayochukwu James <anayochukwujames11@gmail.com>
2024-08-27 17:36:58 +0100

    update initial commit

---
+++ src/main/java/com/james/paginationandsorting/model/Product.java
@@ -0,0 +1,23 @@
+package com.james.paginationandsorting.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false)
+    private int quantity;
+}
